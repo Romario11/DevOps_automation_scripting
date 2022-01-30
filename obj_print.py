51b08b1e9778f7cebf476c7840cad2b6c25521da
Roman Savchuk <rsavchu@softserveinc.com>
2022-01-31 00:30:34 +0200

    update python script to stable version

--- obj_print.py
+++ obj_print.py
@@ -3,21 +3,58 @@ from urllib.parse import urlparse
 import re
 import boto3
 
-print ("Number of arguments:", len(sys.argv))
-print ("Argument List:", str(sys.argv))
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+def out_red(text):
+    print("\033[31m {}" .format(text))
+    print("\033[37m {}" .format(''))
+def out_green(text):
+    print("\033[33m {}" .format('Content from s3 object is:\n'))
+    print("\033[32m {}" .format(text))
+    print("\033[37m {}" .format(''))
+def out_yellow(text):
+    print("\033[33m {}" .format(text))
+    print("\033[37m {}" .format(''))
+def out_blue(text):
+    print("\033[34m {}" .format(text))
+    print("\033[37m {}" .format(''))
 
-#ParseResult(scheme='https', netloc='my-bucket-for-python-script.s3.eu-central-1.amazonaws.com', path='/my-folder/my-text.txt', params='', query='', fragment='')
-o = urlparse("https://my-bucket-for-python-script.s3.eu-central-1.amazonaws.com/my-folder/my-text.txt")
-print(o.netloc)
-print(o.path)
+
+if ( len(sys.argv) < 2 or (not bool(re.search('^https://.+',sys.argv[1])))):
+    out_red('Something wrong with argument. Please enter correct URL')
+    exit()
 
 
-m = re.search('^([a-z]+(-?[a-z]+)+)', o.netloc)
+inputURL = str(sys.argv[1])
 
-object = s3.Object('my-bucket-for-python-script','my-folder/my-text.txt')
-print(object.get()['Body'].read())
-print(m.group(0))
-input("press")
+
+def parseOldS3UrlFormat(Url):
+    parsedObject = urlparse(Url)
+    backetName = re.match('^([a-z])+(([a-z])?([0-9])?(-)?)+',parsedObject.path[1:]).group(0)
+    objectKey = re.sub('^([a-z])+(([a-z])?([0-9])?(-)?)+/','',parsedObject.path[1:])
+    return {'backetName' : backetName, 'objectKey': objectKey}
+
+
+def parseNewS3UrlFormat(Url):
+    parsedObject = urlparse(Url)
+    backetName = re.match('^([a-z])+(([a-z])?([0-9])?(-)?)+', parsedObject.netloc).group(0)
+    objectKey = parsedObject.path[1:]
+    return {'backetName' : backetName, 'objectKey': objectKey}
+
+
+def getObjectContent(listOfObjectData):
+    s3 = boto3.resource('s3')
+    S3object = s3.Object(listOfObjectData.get('backetName'),listOfObjectData.get('objectKey'))
+    return S3object.get()['Body'].read().decode()
+    
+
+if ( bool(re.search('^(https://s3\.)',inputURL))):
+    listOfObjectData = parseOldS3UrlFormat(inputURL)
+    out_green(getObjectContent(listOfObjectData))
+else:
+    listOfObjectData = parseNewS3UrlFormat(inputURL)
+    try:
+        out_green(getObjectContent(listOfObjectData))
+    except Exception as e:
+        out_red("Error! Wrong URL or AWS CLI not configured")
+        exit()
+
+out_blue('The script completed successfully')
